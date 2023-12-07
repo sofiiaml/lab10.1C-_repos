861108a213c0aa5c45a5adb507fdca000fea027a
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-12-08 00:47:01 +0200

    fw

---
+++ PR10.1/PR10.1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <fstream>
+#include <map>
+
+using namespace std;
+
+void Count(const string& filename, map<char, int>& counts) {
+    ifstream file(filename);
+
+    if (!file.is_open()) {
+        cout << "Error during opening file" << endl;
+        return;
+    }
+
+    char s;
+    while (file.get(s)) {
+        if (s == '+' || s == '-' || s == '=') {
+            counts[s]++;
+        }
+    }
+
+    file.close();
+}
+
+int main() {
+    string filename;
+    cout << "Enter the filename: ";
+    cin >> filename;
+
+    map<char, int> occurrences;
+    Count(filename, occurrences);
+
+    // Виведення результатів
+    cout << "Occurrences in file '" << filename << "':" << endl;
+    cout << "Symbol '+' " << occurrences['+'] << " times." << endl;
+    cout << "Symbol '-' " << occurrences['-'] << " times." << endl;
+    cout << "Symbol '=' " << occurrences['='] << " times." << endl;
+
+    return 0;
+}
